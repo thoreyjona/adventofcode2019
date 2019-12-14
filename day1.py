d0c36aca3ce7e7b1db27331284c5b1eee95f6c55
thoreyjona <thorey@travelade.com>
2019-12-14 22:50:12 +0000

    Day 1 part2

--- day1.py
+++ day1.py
@@ -1,17 +1,36 @@
 import math
 
-input_file = open('input-day1.txt', 'r')
+# Part 1
+
+input_file = open('input-day1-part1.txt', 'r')
 input = []
-fuel = []
+fuels = []
 accumulated_fuel = 0
 
 for line in input_file:
     input.append(int(line.strip('\n')))
 
 for mass in input:
-    fuel.append((math.floor(mass / 3)) - 2)
+    fuels.append((math.floor(mass / 3)) - 2)
 
-for fuel_element in fuel:
-    accumulated_fuel += fuel_element
+for fuel in fuels:
+    accumulated_fuel += fuel
 
 print(accumulated_fuel)
+
+# Part 2
+
+fuels_part2 = []
+acc_fuels = 0
+
+for fuel in fuels:
+    acc = fuel
+    while fuel > 0:
+        fuel = (math.floor(fuel / 3)) - 2
+        if (fuel > 0):
+            acc += fuel
+    fuels_part2.append(acc)
+
+for fuel in fuels_part2:
+    acc_fuels += fuel
+print(acc_fuels)
